21e8b0de0361272372931a7842cae39b8712f75f
lalit nath <lalitwebd@bigwigmediadigital.com>
2026-08-03 12:26:12 +0530

    admin whatsapp order nootification

--- services/notification/sendWhatsapp.js
+++ services/notification/sendWhatsapp.js
@@ -43,6 +43,30 @@ const sendWhatsappOrderConfirmation = async (order) => {
       from: process.env.TWILIO_WHATSAPP_NUMBER,
       to: `whatsapp:+91${order.customerPhone}`,
     });
+
+    await client.messages.create({
+      contentSid: process.env.TWILIO_ADMIN_WHATSAPP_TEMPLATE_SID,
+      contentVariables: JSON.stringify({
+      1: order.orderNumber,
+      2: order.customerName,
+      3: order.customerPhone,
+      4: String(order.pricing.total),
+      5: order.payment.method.toUpperCase(),
+      6: `${order.items.length} item${order.items.length > 1 ? "s" : ""}`,
+    }),
+      from: process.env.TWILIO_WHATSAPP_NUMBER,
+      to: `whatsapp:${process.env.ADMIN_PHONE}`,
+    });
+        // 🛍️ New Order Received on Rehnoor Jewels
+
+        // Order Number: {{1}}
+        // Customer Name: {{2}}
+        // Customer Phone: {{3}}
+        // Order Amount: ₹{{4}}
+        // Payment Method: {{5}}
+        // Total Items: {{6}}
+
+        // Please check the admin panel for full order details.
     console.log(`Whatsapp order confirmation sent to ${order.customerPhone}`);
   } catch (err) {
     console.error("Whatsapp Error:", err);
